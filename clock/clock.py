7ba588389edc15bb88ae423423cff5c365b245e4
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2022-07-05 20:35:30 +0900

    delete print

--- clock/clock.py
+++ clock/clock.py
@@ -6,14 +6,12 @@ def today():
     #年月日
     today=datetime.now()
     d1=today.strftime("20%y年%m月%d日")
-    print(d1)
     return d1
 
 def now():
     #時分秒
     now=datetime.now()
     ns=now.strftime("%H時%M分%S秒")
-    print(ns)
     return ns
 
 def timezone():
@@ -22,5 +20,4 @@ def timezone():
     nowtz=now.astimezone()
     heretz=nowtz.tzinfo
     tzn=heretz.tzname(nowtz)
-    print("your time zone is ", tzn)
     return tzn
